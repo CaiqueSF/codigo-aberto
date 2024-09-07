4beec166c212ee1de1f35cd2e36b7861be65fb62
CaiqueSF <caiquedesousaferreira@gmail.com>
2024-09-06 22:40:10 -0300

    ImersãoAlura(Aula_5)

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# template-repo
-Template para criar repositórios
+# Tecnologia da Informação
+
+## Descrição
+
+O projeto **"Tecnologia da Informação"** é uma aplicação web desenvolvida para ajudar os usuários a encontrar informações sobre diversas linguagens de programação. O projeto permite que os usuários pesquisem por linguagens e visualizem informações relevantes sobre elas. A aplicação é responsiva e possui um design moderno com um fundo de imagem e uma interface amigável.
+
+## Estrutura do Projeto
+
+O projeto é estruturado da seguinte forma:
+
+. ├── index.html ├── style.css ├── app.js ├── dados.js └── images/ └── img1.jpg
+
+### `index.html`
+
+O arquivo HTML principal que estrutura a página e inclui referências aos arquivos CSS e JavaScript. Contém:
+
+-   Um campo de pesquisa para o usuário digitar a linguagem de programação.
+-   Um botão para iniciar a pesquisa.
+-   Uma seção para exibir os resultados da pesquisa.
+
+### `style.css`
+
+O arquivo CSS que define o estilo visual da aplicação, incluindo:
+
+-   Fonte padrão.
+-   Estilização de títulos e textos.
+-   Design dos botões e da caixa de resultados.
+-   Responsividade para diferentes tamanhos de tela.
+
+### `app.js`
+
+O arquivo JavaScript principal que contém a lógica para pesquisar e exibir os resultados. Inclui:
+
+-   Função `pesquisar()` que processa o valor do campo de pesquisa e atualiza a seção de resultados.
+
+### `dados.js`
+
+Um arquivo JavaScript que deve conter os dados a serem pesquisados e exibidos. Este arquivo deve exportar uma lista de dados com informações sobre as linguagens de programação.
+
+### `images/img1.jpg`
+
+Imagem de fundo utilizada na aplicação.
+
+## Funcionalidades
+
+-   **Busca**: Permite que os usuários pesquisem por linguagens de programação.
+-   **Exibição de Resultados**: Mostra informações sobre as linguagens que correspondem à pesquisa.
+-   **Mensagens de Erro**: Exibe mensagens quando a pesquisa não retorna resultados ou quando o campo de pesquisa está vazio.
+
+## Como Executar o Projeto
+
+1. Clone o repositório:
+
+    ```bash
+    git clone https://github.com/seu-usuario/seu-repositorio.git
+    ```
+
+2. Navegue para o diretório do projeto:
+
+    ```bash
+    cd seu-repositorio
+    ```
+
+3. Abra o arquivo `index.html` em um navegador.
+
+## Contribuindo
+
+Se você deseja contribuir para o projeto, siga estas etapas:
+
+1. Faça um fork do repositório.
+
+2. Crie uma nova branch:
+
+    ```bash
+    git checkout -b minha-nova-feature
+    ```
+
+3. Faça suas alterações e commit:
+
+    ```bash
+    git add .
+    git commit -m "Adiciona nova feature"
+    ```
+
+4. Envie suas alterações para o repositório remoto:
+
+    ```bash
+    git push origin minha-nova-feature
+    ```
+
+5. Abra um Pull Request no GitHub.
+
+## Contato
+
+-   **E-mail**: caiquedesousaferreira@gmail.com
+-   **LinkedIn**: [Caíque de S. Ferreira](https://www.linkedin.com/in/ca%C3%ADque-de-s-ferreira-48105b18b/)
+-   **GitHub**: [CaiqueSF](https://github.com/CaiqueSF)
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let listaDados = [
     {
         linguagem: "Python - A Versatilidade em um Código Elegante",
         descricao: "Python é uma linguagem de programação fácil de aprender e muito versátil. É perfeita para quem está começando a programar ou para quem busca construir projetos complexos como modelos de inteligência artificial. Com Python, você pode automatizar tarefas, analisar dados e criar sistemas inteligentes. Sua sintaxe clara e a vasta quantidade de bibliotecas disponíveis tornam a programação em Python uma experiência prazerosa e produtiva.",
-        link: "https://www.python.org/"
+        link: "https://www.python.org/",
+        tags: "estatística análise de dados data science machine learning"
     },
     {
         linguagem: "C# - A Potência da Plataforma .NET",
         descricao: "Ideal para criar aplicações robustas e escaláveis, especialmente para Windows e jogos. Com o C# e a plataforma .NET, você pode desenvolver desde simples aplicativos até grandes jogos utilizando a Unity. A linguagem oferece um ecossistema rico de ferramentas e bibliotecas, tornando-a uma excelente opção para projetos profissionais.",
-        link: "https://learn.microsoft.com/pt-br/dotnet/csharp/"
+        link: "https://learn.microsoft.com/pt-br/dotnet/csharp/",
+        tags: ".net asp.net microsoft."
     },
     {
         linguagem: "JavaScript - A Linguagem do Navegador e Além",
         descricao: "Essencial para o desenvolvimento web, o JavaScript permite criar sites dinâmicos e interativos. Mas sua aplicação vai além do front-end: com Node.js, você pode construir servidores web e criar aplicações completas. Frameworks como React e Angular facilitam a criação de interfaces de usuário complexas e modernas. A comunidade ativa e a versatilidade da linguagem a tornam uma escolha popular para diversos projetos.",
-        link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+        tags: "js"
     },
     {
         linguagem: "Java - A Linguagem do Mundo Corporativo",
@@ -22,27 +25,32 @@ let listaDados = [
     {
         linguagem: "C++ - A Linguagem de Alto Desempenho",
         descricao: "C++ é uma linguagem poderosa, utilizada em desenvolvimento de sistemas, jogos, software de engenharia e muito mais. Oferece alto controle sobre o hardware e é ideal para aplicações que exigem desempenho máximo.",
-        link: "https://isocpp.org/"
+        link: "https://isocpp.org/",
+        tags: "jogos"
     },
     {
         linguagem: "Go - A Linguagem Simples e Eficiente",
         descricao: "Go, desenvolvida pelo Google, é uma linguagem compilada, rápida e concisa, ideal para desenvolvimento de sistemas distribuídos, aplicações em nuvem e serviços da web.",
-        link: "https://go.dev/"
+        link: "https://go.dev/",
+        tags: "google"
     },
     {
         linguagem: "Ruby - A Linguagem da Programação Feliz",
         descricao: "Ruby é uma linguagem interpretada, de alto nível e orientada a objetos, conhecida por sua sintaxe elegante e expressiva. É amplamente utilizada em desenvolvimento web com o framework Ruby on Rails.",
-        link: "https://www.ruby-lang.org/"
+        link: "https://www.ruby-lang.org/",
+        tags: "rails"
     },
     {
         linguagem: "Swift - A Linguagem do Futuro para iOS e macOS",
         descricao: "Swift é uma linguagem de programação desenvolvida pela Apple para criar aplicativos para iOS, macOS, watchOS e tvOS. Conhecida por sua segurança e performance, Swift é uma ótima opção para desenvolvedores iOS.",
-        link: "https://swift.org/"
+        link: "https://swift.org/",
+        tags: "ios macos watchOS tvOS"
     },
     {
         linguagem: "PHP - A Linguagem do WordPress",
         descricao: "PHP é uma linguagem interpretada, especialmente projetada para desenvolvimento web. É amplamente utilizada em sistemas de gerenciamento de conteúdo como o WordPress.",
-        link: "https://www.php.net/"
+        link: "https://www.php.net/",
+        tags: "wordpress"
     },
     {
         linguagem: "Rust - A Linguagem Segura e Concorrente",
@@ -52,22 +60,26 @@ let listaDados = [
     {
         linguagem: "Kotlin - A Linguagem Moderna para o Desenvolvimento Android",
         descricao: "Kotlin é uma linguagem moderna e concisa, projetada para interoperar com Java. É amplamente utilizada no desenvolvimento Android e back-end.",
-        link: "https://kotlinlang.org/"
+        link: "https://kotlinlang.org/",
+        tags: "android"
     },
     {
         linguagem: "TypeScript - JavaScript com Tipagem Estática",
         descricao: "TypeScript é um superconjunto do JavaScript que adiciona tipagem estática, oferecendo maior segurança e escalabilidade para projetos JavaScript.",
-        link: "https://www.typescriptlang.org/"
+        link: "https://www.typescriptlang.org/",
+        tags: "ts"
     },
     {
         linguagem: "Scala - A Linguagem Funcional para a JVM",
         descricao: "Scala é uma linguagem de programação funcional que roda na JVM, combinando programação funcional e orientada a objetos.",
-        link: "https://www.scala-lang.org/"
+        link: "https://www.scala-lang.org/",
+        tags: "jvm"
     },
     {
         linguagem: "R - Para Estatística e Análise de Dados",
         descricao: "R é uma linguagem e ambiente de software livre para computação estatística e gráficos. É amplamente utilizada em data science e machine learning.",
-        link: "https://www.r-project.org/"
+        link: "https://www.r-project.org/",
+        tags: "estatística análise de dados data science machine learning"
     },
     {
         linguagem: "Perl - A Linguagem da Ferramenta",
@@ -77,7 +89,8 @@ let listaDados = [
     {
         linguagem: "Lua - A Linguagem Leve e Embutível",
         descricao: "Lua é uma linguagem de scripting leve e extensível, frequentemente usada em jogos e aplicações embarcadas.",
-        link: "https://www.lua.org/"
+        link: "https://www.lua.org/",
+        tags: "jogos embarcados"
     },
     {
         linguagem: "Haskell - A Linguagem Funcional Pura",
@@ -87,16 +100,19 @@ let listaDados = [
     {
         linguagem: "Dart - A Linguagem para Flutter",
         descricao: "Dart é uma linguagem de programação cliente-servidor desenvolvida pelo Google. É usada principalmente para construir aplicativos móveis com o framework Flutter, mas também pode ser usada para desenvolvimento web.",
-        link: "https://dart.dev/"
+        link: "https://dart.dev/",
+        tags: "google flutter"
     },
     {
         linguagem: "Julia - Para Ciência de Dados e Computação Científica",
         descricao: "Julia é uma linguagem de programação de alto nível, de alto desempenho, dinâmica e de propósito geral. É especialmente projetada para computação numérica e científica.",
-        link: "https://julialang.org/"
+        link: "https://julialang.org/",
+        tags: "computacao cientifica"
     },
     {
         linguagem: "Elixir - A Linguagem Funcional para a Máquina Virtual Erlang",
         descricao: "Elixir é uma linguagem funcional que roda na máquina virtual Erlang, construída para construir sistemas distribuídos e tolerantes a falhas.",
-        link: "https://elixir-lang.org/"
+        link: "https://elixir-lang.org/",
+        tags: "erlang"
     }
 ];
